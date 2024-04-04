78234b2d719cf583db1fa6842bca0e4dd0a10f5d
mukhammed.codes <muhammedtoichubai@gmail.com>
2024-04-04 11:18:00 +0600

    initial commit

---
+++ src/main/java/lms/LmsB12Application.java
@@ -0,0 +1,13 @@
+package lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsB12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmsB12Application.class, args);
+    }
+
+}
