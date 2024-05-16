935ef60768fee746492caf9d562eefaf557dac69
mukhammed.codes <muhammedtoichubai@gmail.com>
2024-05-16 12:12:08 +0600

    refactoring the application.yml file

--- src/main/java/lms/entities/User.java
+++ src/main/java/lms/entities/User.java
@@ -1,25 +1,9 @@
 package lms.entities;
 
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Column;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.persistence.*;
 import lms.enums.Role;
 import lms.enums.Type;
-import lms.validation.email.EmailValidation;
-import lms.validation.phoneNumber.PhoneNumberValidation;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
