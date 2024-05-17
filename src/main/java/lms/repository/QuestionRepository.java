e1d97814766df85adcdededf3075b6b82c55c4e7
Mukhammed Toichubai uulu <98472046+MuhammedToichubai@users.noreply.github.com>
2024-05-17 12:34:34 +0600

    Update QuestionRepository.java

--- src/main/java/lms/repository/QuestionRepository.java
+++ src/main/java/lms/repository/QuestionRepository.java
@@ -10,7 +10,4 @@ import java.util.List;
 public interface QuestionRepository extends JpaRepository<Question, Long> {
     @Query("delete from Question q where q.id = :questionId")
     void deleteQuestionById(Long questionId);
-
-//    @Query("select new lms.dto.response.QuestionResponse(q.title,q.point,q.questionType,q.options) from Question q")
-//    List<QuestionResponse> findByTestId(Long id);
 }
