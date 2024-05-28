e4bdc51c6b06040eb704d852bece434cb349ea34
macbook <ajybeksadykov@gmail.com>
2024-05-28 14:09:50 +0600

    cleand code

--- src/main/java/lms/repository/LinkRepository.java
+++ src/main/java/lms/repository/LinkRepository.java
@@ -4,19 +4,13 @@ import lms.dto.response.LinkResponse;
 import lms.entities.Lesson;
 import lms.entities.Link;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.util.List;
-
 public interface LinkRepository extends JpaRepository<Link, Long> {
     @Query("select new lms.dto.response.LinkResponse(l.id, l.title, l.url) from Link l where l.lesson.id = :lessonId")
     Page<LinkResponse> findAllLinksByLesson(@Param("lessonId")Long lessonId, Pageable pageable);
     @Query("select s from Lesson s join s.videos v where v.id =:id")
     Lesson findByVideoId(@Param("id") Long id);
-//    @Query("select s from Lesson s join s.presentations p where .id =:id")
-//    Lesson findLessonByPresentationId(@Param("id") Long id);
 }
--- src/main/java/lms/repository/QuestionRepository.java
+++ src/main/java/lms/repository/QuestionRepository.java
@@ -1,13 +1,6 @@
 package lms.repository;
 
-import lms.dto.response.QuestionResponse;
 import lms.entities.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
-
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    @Query("delete from Question q where q.id = :questionId")
-    void deleteQuestionById(Long questionId);
 }
