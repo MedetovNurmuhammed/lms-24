536c9c199a0e0439277a0e132ee1c7659a95aff7
Gulum <gulumkanusonkyzy@gmail.com>
2024-06-17 14:08:51 +0600

    refactor phoneNuber validation and createPassword

--- src/main/java/lms/validation/phoneNumber/PhoneNumberValidator.java
+++ src/main/java/lms/validation/phoneNumber/PhoneNumberValidator.java
@@ -6,11 +6,24 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class PhoneNumberValidator implements ConstraintValidator<PhoneNumberValidation, String> {
+
     @Override
-    public boolean isValid(String phoneNumber, ConstraintValidatorContext constraintValidatorContext) {
-        log.error("Не корректный тел номер!");
-        return phoneNumber.startsWith("+996")
-                && phoneNumber.length() == 13
-                && phoneNumber.substring(4).matches("^[0-9]+$");
+    public boolean isValid(String phoneNumber, ConstraintValidatorContext context) {
+        if (phoneNumber == null) {
+            return true;
+        }
+
+        boolean isValid = phoneNumber.startsWith("+") && phoneNumber.substring(1).matches("^[0-9]+$");
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(
+                    "Номер телефона должен состоять только из цифр и символа '+'"
+            ).addConstraintViolation();
+            log.error("Неверный формат номера телефона: {}", phoneNumber);
+        }
+
+        return isValid;
     }
 }
+
