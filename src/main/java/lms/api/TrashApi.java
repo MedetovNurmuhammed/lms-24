332fee18e4e125426d5461e5d83c11be0ac0adee
mukhammed.codes <muhammedtoichubai@gmail.com>
2024-06-17 10:52:47 +0600

    Trash API is complete

--- src/main/java/lms/api/TrashApi.java
+++ src/main/java/lms/api/TrashApi.java
@@ -1,6 +1,7 @@
 package lms.api;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lms.dto.response.AllTrashResponse;
 import lms.dto.response.SimpleResponse;
 import lms.service.TrashService;
@@ -8,31 +9,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequestMapping("/api/trash")
+@Tag(name = "TRASH MANAGEMENT!")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*",maxAge = 3600)
 @PreAuthorize("hasAnyAuthority('ADMIN','INSTRUCTOR')")
-public class MyTrashApi {
+public class TrashApi {
     private final TrashService trashService;
 
-    @Operation(summary = "Чтобы получить все удаленные, но не очищенные данные. Используйте токен пользователя, чтобы получить",
-            description = " Авторизация: администратор и инструктор!")
+    @Operation(summary = "Получить все объекты из корзины.", description = " Авторизация: администратор и инструктор!")
     @GetMapping("/findAll")
     public AllTrashResponse findAll(@RequestParam(required = false, defaultValue = "1") int page,
                                     @RequestParam(required = false, defaultValue = "6") int size) {
         return trashService.findAll(page, size);
     }
 
-    @Operation(summary = "Возвращать из корзины.", description = "Авторизация: администратор и инструктор!")
-    @PreAuthorize("hasAnyAuthority('ADMIN','INSTRUCTOR')")
+    @Operation(summary = "Восстановить объект из корзины.", description = "Авторизация: администратор и инструктор!")
     @DeleteMapping("/restore/{trashID}")
     public SimpleResponse restoreData(@PathVariable Long trashID) {
         return trashService.restoreData(trashID);
     }
 
-    @Operation(summary = "Удалить из корзины.")
+    @Operation(summary = "Удалить объект из корзины.", description = "Авторизация: администратор и инструктор!")
     @DeleteMapping("{trashID}")
     public SimpleResponse deleteTrash(@PathVariable Long trashID){
         return trashService.deleteData(trashID);
